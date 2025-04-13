5f03a038eeb1f3d6c4dcf038533a262f1b3a4ec1
IvanReshetnikovGit <ivanreshet@icloud.com>
2025-04-13 01:03:50 +0000

    gitignore change

--- Core/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3108e25daaa0394cf9250cb5c7d6659ef5b2272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+febca5627eb0e4bebb323a5a572c6711d08f660a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
